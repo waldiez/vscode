c04f054489df46a3e84d50be1291afe577d67e1b
Lazaros Toumanidis <laztoum@protonmail.com>
2024-12-03 23:27:06 +0200

    update tests

--- src/test/suite/beforeTests.ts
+++ src/test/suite/beforeTests.ts
@@ -1,14 +1,10 @@
 import { PythonExtension } from '@vscode/python-extension';
-import { spawnSync } from 'child_process';
 import * as vscode from 'vscode';
 
 export async function beforeTests() {
     await vscode.extensions.getExtension('ms-python.python')?.activate();
     await waitForPythonEnvironments();
     const api = await PythonExtension.api();
-    // also pip install waldiez in the environment
-    // get the highest version of the environments (that is >= 3.10 and <= 3.13)
-    // and install waldiez in that environment
     const highestVersion = api.environments.known
         .filter(
             env =>
@@ -22,49 +18,10 @@ export async function beforeTests() {
             }
             return b.version.minor - a.version.minor;
         })[0];
-    if (highestVersion) {
-        const executable = highestVersion.path;
-        console.log(`Installing waldiez using ${executable}`);
-        const pythonVersion = spawnSync(executable, ['--version']);
-        console.log(pythonVersion.stdout.toString());
-        const output = spawnSync(
-            executable,
-            ['-m', 'pip', 'install', '--upgrade', 'pip'],
-            {
-                stdio: 'pipe',
-                shell: process.platform === 'win32'
-            }
-        );
-        if (output.status !== 0) {
-            console.error(output.stderr.toString());
-            throw new Error('Failed to upgrade pip');
-        } else {
-            console.log(output.stdout.toString());
-            console.log('Pip upgraded successfully');
-        }
-        const output2 = spawnSync(
-            executable,
-            [
-                '-m',
-                'pip',
-                'install',
-                '--upgrade',
-                '--break-system-packages',
-                'waldiez'
-            ],
-            {
-                stdio: 'pipe',
-                shell: process.platform === 'win32'
-            }
-        );
-        if (output2.status !== 0) {
-            console.error(output2.stderr.toString());
-            throw new Error('Failed to install waldiez');
-        } else {
-            console.log(output2.stdout.toString());
-            console.log('Waldiez installed successfully');
-        }
+    if (!highestVersion) {
+        throw new Error('No suitable Python environment found');
     }
+    console.log(`Using Python environment: ${highestVersion.path}`);
 }
 
 const waitForPythonEnvironments = async () => {
