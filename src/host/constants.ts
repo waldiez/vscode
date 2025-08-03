f672a8aeec9a8d43a3652deaf257b776dc3a401c
Lazaros Toumanidis <laztoum@waldiez.io>
2025-08-03 12:58:22 +0300

    bump to 0.5.8

--- src/host/constants.ts
+++ src/host/constants.ts
@@ -2,7 +2,7 @@
  * SPDX-License-Identifier: Apache-2.0
  * Copyright 2024 - 2025 Waldiez & contributors
  */
-export const MINIMUM_REQUIRED_WALDIEZ_PY_VERSION = "0.5.7";
+export const MINIMUM_REQUIRED_WALDIEZ_PY_VERSION = "0.5.8";
 export const TIME_TO_WAIT_FOR_INPUT = 120000; // at most 2 minutes
 // Command IDs to register
 export const CONVERT_TO_PYTHON = "waldiez.vscode.toPython";
