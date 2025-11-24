17ecdbe97d94a3ed20aca003b4f94d9d3bbd0cd3
Lazaros Toumanidis <laztoum@waldiez.io>
2025-11-24 11:56:35 +0200

    bump to 0.6.6

--- src/host/constants.ts
+++ src/host/constants.ts
@@ -3,7 +3,7 @@
  * Copyright 2024 - 2025 Waldiez & contributors
  */
 export const MONACO_VERSION = "0.55.1";
-export const MINIMUM_REQUIRED_WALDIEZ_PY_VERSION = "0.6.5";
+export const MINIMUM_REQUIRED_WALDIEZ_PY_VERSION = "0.6.6";
 export const TIME_TO_WAIT_FOR_INPUT = 120000; // at most 2 minutes
 // Command IDs to register
 export const CONVERT_TO_PYTHON = "waldiez.vscode.toPython";
